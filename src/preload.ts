c6ae7d064bc1ae3b36775a67913402d00fd4c4d6
sprout2000 <k_konno1983@yahoo.co.jp>
2020-03-25 13:28:17 +0900

    Add ipcRenderer.send(): void

--- src/preload.ts
+++ src/preload.ts
@@ -4,6 +4,9 @@ contextBridge.exposeInMainWorld('ipcRenderer', {
   invoke: (channel: string, ...args: any[]): Promise<any> => {
     return ipcRenderer.invoke(channel, ...args);
   },
+  send: (channel: string, ...args: any[]): void => {
+    ipcRenderer.send(channel, ...args);
+  },
   on: (
     channel: string,
     listener: (event: Electron.IpcRendererEvent, ...args: any[]) => void
