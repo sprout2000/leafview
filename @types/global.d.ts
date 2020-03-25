c8293eda5100b736051139bac75bb7f0caf737a0
sprout2000 <k_konno1983@yahoo.co.jp>
2020-03-25 13:29:23 +0900

    Add send(): void

--- @types/global.d.ts
+++ @types/global.d.ts
@@ -6,6 +6,7 @@ declare global {
 
 export interface Sandbox {
   invoke(channel: string, ...args: any[]): Promise<any>;
+  send(channel: string, ...args: any[]): void;
   on(
     channel: string,
     listener: (event: Electron.IpcRendererEvent, ...args: any[]) => void
