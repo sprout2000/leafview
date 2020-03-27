e24833c57c8f628b17c3dbe190a33d084ac15c83
sprout2000 <k_konno1983@yahoo.co.jp>
2020-03-27 17:55:10 +0900

    console.log = log.log

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,7 @@ import en from './locales/en.json';
 import ja from './locales/ja.json';
 import createMenu from './menu';
 
+console.log = log.log;
 autoUpdater.logger = log;
 log.info('App starting...');
 
