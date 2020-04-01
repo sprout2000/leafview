e455aad8572c2190152a0bc91056693dd5207ef6
sprout2000 <k_konno1983@yahoo.co.jp>
2020-04-01 18:21:36 +0900

    lint 2 files

--- @types/global.d.ts
+++ @types/global.d.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 declare global {
   interface Window {
     ipcRenderer: Sandbox;
--- src/preload.ts
+++ src/preload.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { contextBridge, ipcRenderer } from 'electron';
 
 contextBridge.exposeInMainWorld('ipcRenderer', {
