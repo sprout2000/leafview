1ac5f1cb1a8b5ba1d790138791031fa4038d6746
sprout2000 <k_konno1983@yahoo.co.jp>
2020-06-04 09:40:50 +0900

    perf: create myAPI

--- src/preload.ts
+++ src/preload.ts
@@ -1,20 +1,41 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { contextBridge, ipcRenderer } from 'electron';
 
-contextBridge.exposeInMainWorld('ipcRenderer', {
-  invoke: (channel: string, ...args: any[]): Promise<any> => {
-    return ipcRenderer.invoke(channel, ...args);
-  },
-  send: (channel: string, ...args: any[]): void => {
-    ipcRenderer.send(channel, ...args);
-  },
-  on: (
-    channel: string,
-    listener: (event: Electron.IpcRendererEvent, ...args: any[]) => void
-  ) => {
-    ipcRenderer.on(channel, listener);
-  },
-  removeAllListeners: (channel: string) => {
-    ipcRenderer.removeAllListeners(channel);
-  },
+contextBridge.exposeInMainWorld('myAPI', {
+  platform: async (): Promise<boolean> => await ipcRenderer.invoke('platform'),
+
+  mimecheck: async (filepath: string): Promise<boolean> =>
+    await ipcRenderer.invoke('mime-check', filepath),
+
+  history: (filepath: string) => ipcRenderer.send('file-histoy', filepath),
+
+  dirname: async (filepath: string): Promise<string> =>
+    await ipcRenderer.invoke('dirname', filepath),
+
+  readdir: async (dirpath: string): Promise<void | string[]> =>
+    await ipcRenderer.invoke('readdir', dirpath),
+
+  moveToTrash: async (filepath: string): Promise<boolean> =>
+    await ipcRenderer.invoke('move-to-trash', filepath),
+
+  openDialog: async (): Promise<string | void> =>
+    ipcRenderer.invoke('open-dialog'),
+
+  updateTitle: async (filepath: string): Promise<void> =>
+    ipcRenderer.invoke('update-title', filepath),
+
+  menuNext: (listener: () => Promise<void>) =>
+    ipcRenderer.on('menu-next', listener),
+  removeMenuNext: () => ipcRenderer.removeAllListeners('menu-next'),
+
+  menuPrev: (listener: () => Promise<void>) =>
+    ipcRenderer.on('menu-prev', listener),
+  removeMenuPrev: () => ipcRenderer.removeAllListeners('menu-prev'),
+
+  menuRemove: (listener: () => Promise<void>) =>
+    ipcRenderer.on('menu-remove', listener),
+  removeMenuRemove: () => ipcRenderer.removeAllListeners('menu-remove'),
+
+  menuOpen: (listener: (_e: Event, filepath: string) => Promise<void>) =>
+    ipcRenderer.on('menu-open', listener),
+  removeMenuOpen: () => ipcRenderer.removeAllListeners('menu-open'),
 });
