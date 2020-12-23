d7aa20d06ee8006e874c0ae544a0499171bcc015
sprout2000 <wassabie64@gmail.com>
2020-12-24 08:26:44 +0900

    chore: update targets for arm64-mac

--- scripts/build.ts
+++ scripts/build.ts
@@ -6,46 +6,22 @@ dotenv.config();
 build({
   config: {
     productName: 'LeafView',
-    artifactName: '${productName}-${version}-${platform}.${ext}',
+    artifactName: '${productName}-${version}-${platform}-${arch}.${ext}',
     copyright: 'Copyright (C) 2020 sprout2000.',
     files: ['dist/**/*'],
-    publish: [
-      {
-        provider: 'github',
-        releaseType: 'release',
-      },
-    ],
     directories: {
       buildResources: 'assets',
       output: 'release',
     },
     asar: true,
     asarUnpack: ['dist/preload.js'],
-    win: {
-      icon: 'assets/icon.ico',
-      target: ['appx'],
-      publisherName: 'sprout2000',
-      fileAssociations: [
-        {
-          ext: ['bmp', 'gif', 'jpeg', 'jpg', 'png', 'ico', 'svg', 'webp'],
-          description: 'Image files',
-        },
-      ],
-    },
-    appx: {
-      applicationId: 'sprout2000.LeafView',
-      backgroundColor: '#1d3557',
-      displayName: 'LeafView',
-      showNameOnTiles: true,
-      languages: ['EN-US', 'JA-JP', 'CS-CZ'],
-      identityName: process.env.IDENTITY_NAME,
-      publisher: process.env.PUBLISHER,
-      publisherDisplayName: 'sprout2000',
-    },
     mac: {
       appId: process.env.APP_BUNDLE_ID,
       category: 'public.app-category.photography',
-      target: 'default',
+      target: {
+        target: 'default',
+        arch: 'universal',
+      },
       icon: 'assets/icon.icns',
       extendInfo: {
         CFBundleName: 'LeafView',
