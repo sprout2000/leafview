e2e98d6af412cbb70409e7a54a75c06d5b2bbce4
sprout2000 <wassabie64@gmail.com>
2021-03-02 11:55:40 +0900

    chore: update searchDevtools()

--- src/searchDevtools.ts
+++ src/searchDevtools.ts
@@ -3,10 +3,14 @@ import fs from 'fs';
 import path from 'path';
 
 export const searchDevtools = async (): Promise<string> => {
+  const isWin32 = os.platform() === 'win32';
   const isDarwin = os.platform() === 'darwin';
+
   const extDir = isDarwin
     ? '/Library/Application Support/Google/Chrome/Default/Extensions/fmkadmapgofadopljbjfkapdkoienihi'
-    : '\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Extensions';
+    : isWin32
+    ? '\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Extensions'
+    : '/.config/google-chrome/Default/Extensions';
   const extPath = path.join(os.homedir(), extDir);
 
   const reactDevtoolsDir = await fs.promises
