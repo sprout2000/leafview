58514ae379c68212560354b99d2e8180b160be20
sprout2000 <wassabie64@gmail.com>
2021-03-25 13:59:08 +0900

    chore: create __mocks__ & __tests__

--- jest.config.ts
+++ jest.config.ts
@@ -1,13 +1,13 @@
 import { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
-  roots: ['<rootDir>/tests'],
+  roots: ['<rootDir>/__tests__'],
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   moduleNameMapper: {
     '\\.(ico|gif|jpe?g|png|svg|ttf|otf|eot|woff?2?)$':
-      '<rootDir>/mocks/fileMock.ts',
-    '\\.(css|scss)$': '<rootDir>/mocks/styleMock.ts',
+      '<rootDir>/__mocks__/fileMock.ts',
+    '\\.(css|scss)$': '<rootDir>/__mocks__/styleMock.ts',
   },
 };
 
