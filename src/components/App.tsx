d285354f692441455a31056a6ce8de75e69eb528
sprout2000 <wassabie64@gmail.com>
2021-06-24 16:33:30 +0900

    refactor: Removed unnecessary React imports.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useCallback, useState, useEffect } from 'react';
+import { useRef, useCallback, useState, useEffect } from 'react';
 
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
