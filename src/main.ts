9941089cd3fb08aabd36d3725c501122376f383e
sprout2000 <wassabie64@gmail.com>
2022-02-13 18:03:20 +0900

    chore: Disabled forceHardReset on development.

--- src/main.ts
+++ src/main.ts
@@ -58,8 +58,6 @@ if (isDevelop) {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   require('electron-reload')(__dirname, {
     electron: path.resolve(__dirname, execPath),
-    forceHardReset: true,
-    hardResetMethod: 'exit',
   });
 }
 
