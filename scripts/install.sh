3cfbae66c3e5d69366131505daf5623b272791b9
sprout2000 <wassabie64@gmail.com>
2022-04-10 15:44:09 +0900

    chore: Updated build process.

--- scripts/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-npm install && npm r eslint prettier eslint-config-prettier \
-  @typescript-eslint/{parser,eslint-plugin} \
-  eslint-plugin-{react,react-hooks} \
-  wait-on electron-reload npm-run-all rimraf
