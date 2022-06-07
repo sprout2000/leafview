5104699fa490396ec0a044e6cd6b441714f19830
sprout2000 <wassabie64@gmail.com>
2022-06-08 08:05:58 +0900

    chore: Fixed build.watch.

--- esbuild.ts
+++ esbuild.ts
@@ -1,10 +1,14 @@
 import { build } from 'esbuild';
 
+const isDev = process.env.NODE_ENV === 'development';
+
 build({
   entryPoints: ['src-main/main.ts', 'src-main/preload.ts'],
   outdir: 'dist',
   bundle: true,
-  minify: true,
   platform: 'node',
   external: ['electron', 'fsevents', 'electron-reload'],
+  watch: isDev,
+  minify: !isDev,
+  sourcemap: isDev,
 });
