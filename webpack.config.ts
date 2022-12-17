dfbf104bd4dbe4f8cecd62241b4154a7a1cc4820
sprout2000 <wassabie64@gmail.com>
2022-12-17 11:06:22 +0900

    style: Updated webpack.config.ts.

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,4 +1,3 @@
-import path from 'node:path';
 import { Configuration } from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import CopyWebpackPlugin from 'copy-webpack-plugin';
@@ -8,16 +7,11 @@ const isDev = process.env.NODE_ENV === 'development';
 
 const common: Configuration = {
   mode: isDev ? 'development' : 'production',
-  node: {
-    __dirname: false,
-    __filename: false,
-  },
   resolve: {
     extensions: ['.js', '.ts', '.jsx', '.tsx', '.json'],
   },
   externals: ['fsevents'],
   output: {
-    path: path.resolve(__dirname, 'dist'),
     publicPath: './',
     filename: '[name].js',
     assetModuleFilename: 'images/[name][ext]',
@@ -31,20 +25,7 @@ const common: Configuration = {
       },
       {
         test: /\.s?css$/,
-        use: [
-          { loader: MiniCssExtractPlugin.loader },
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: isDev,
-              importLoaders: 1,
-            },
-          },
-          {
-            loader: 'sass-loader',
-            options: { sourceMap: isDev },
-          },
-        ],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
       {
         test: /\.png$/,
@@ -54,13 +35,15 @@ const common: Configuration = {
   },
   stats: 'errors-only',
   watch: isDev,
-  devtool: isDev ? 'inline-source-map' : undefined,
+  devtool: isDev ? 'source-map' : undefined,
 };
 
 const main: Configuration = {
   ...common,
   target: 'electron-main',
-  entry: { main: './src/main.ts' },
+  entry: {
+    main: './src/main.ts',
+  },
   plugins: [
     new CopyWebpackPlugin({
       patterns: [
@@ -79,13 +62,17 @@ const main: Configuration = {
 const preload: Configuration = {
   ...common,
   target: 'electron-preload',
-  entry: { preload: './src/preload.ts' },
+  entry: {
+    preload: './src/preload.ts',
+  },
 };
 
 const renderer: Configuration = {
   ...common,
   target: 'web',
-  entry: { index: './src/web/index.tsx' },
+  entry: {
+    index: './src/web/index.tsx',
+  },
   plugins: [
     new MiniCssExtractPlugin(),
     new HtmlWebpackPlugin({
