21d720f78800824b161d743192a551501f2fb57a
sprout2000 <wassabie64@gmail.com>
2022-12-17 13:08:57 +0900

    refactor: Updated props.

--- src/web/components/Grid.tsx
+++ src/web/components/Grid.tsx
@@ -12,33 +12,31 @@ type Props = {
   ) => Promise<void>;
 };
 
-export const Grid = memo(
-  ({ url, imgList, onClickBlank, onClickThumb }: Props) => {
-    const currentRef = useRef<HTMLImageElement>(null);
+export const Grid = memo((props: Props) => {
+  const currentRef = useRef<HTMLImageElement>(null);
 
-    useLayoutEffect(() => {
-      currentRef.current?.scrollIntoView({
-        behavior: 'smooth',
-        block: 'center',
-      });
-    }, []);
+  useLayoutEffect(() => {
+    currentRef.current?.scrollIntoView({
+      behavior: 'smooth',
+      block: 'center',
+    });
+  }, []);
 
-    return (
-      <div className="thumb-container" onClick={onClickBlank}>
-        {imgList.map((item) => (
-          <img
-            key={item}
-            src={item}
-            ref={item === url ? currentRef : null}
-            className={item === url ? 'thumb current' : 'thumb'}
-            onClick={(e) => onClickThumb(e, item)}
-            onDragStart={() => {
-              return false;
-            }}
-          />
-        ))}
-      </div>
-    );
-  }
-);
+  return (
+    <div className="thumb-container" onClick={props.onClickBlank}>
+      {props.imgList.map((item) => (
+        <img
+          key={item}
+          src={item}
+          ref={item === props.url ? currentRef : null}
+          className={item === props.url ? 'thumb current' : 'thumb'}
+          onClick={(e) => props.onClickThumb(e, item)}
+          onDragStart={() => {
+            return false;
+          }}
+        />
+      ))}
+    </div>
+  );
+});
 Grid.displayName = 'Grid';
