d802088b1038b19332e1fb49135f4202078982da
sprout2000 <wassabie64@gmail.com>
2023-04-01 12:25:49 +0900

    test: Update Grid.test.tsx

--- src/web/__tests__/Grid.test.tsx
+++ src/web/__tests__/Grid.test.tsx
@@ -1,14 +1,69 @@
-import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
-
+import { render, screen, fireEvent } from "@testing-library/react";
 import { Grid } from "../Grid";
 
-test("render Grid component", async () => {
-  const spyFn = jest.fn();
-  render(
-    <Grid url="" imgList={[]} onClickBlank={spyFn} onClickThumb={spyFn} />
-  );
+window.HTMLElement.prototype.scrollIntoView = jest.fn();
 
-  await userEvent.click(screen.getByTestId("grid"));
-  expect(spyFn).toHaveBeenCalled();
+describe("Grid", () => {
+  const url = "https://example.com/image1.jpg";
+  const imgList = [
+    "https://example.com/image1.jpg",
+    "https://example.com/image2.jpg",
+    "https://example.com/image3.jpg",
+  ];
+  const onClickBlank = jest.fn();
+  const onClickThumb = jest.fn();
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should render all images in the list", () => {
+    render(
+      <Grid
+        url={url}
+        imgList={imgList}
+        onClickBlank={onClickBlank}
+        onClickThumb={onClickThumb}
+      />
+    );
+
+    const images = screen.getAllByRole("img");
+
+    expect(images.length).toBe(imgList.length);
+  });
+
+  it("should call onClickBlank when clicking on the grid content", () => {
+    render(
+      <Grid
+        url={url}
+        imgList={imgList}
+        onClickBlank={onClickBlank}
+        onClickThumb={onClickThumb}
+      />
+    );
+
+    const gridContent = screen.getByTestId("grid");
+
+    fireEvent.click(gridContent);
+
+    expect(onClickBlank).toHaveBeenCalledTimes(1);
+  });
+
+  it("should call onClickThumb with the clicked item when clicking on a thumbnail", () => {
+    render(
+      <Grid
+        url={url}
+        imgList={imgList}
+        onClickBlank={onClickBlank}
+        onClickThumb={onClickThumb}
+      />
+    );
+
+    const images = screen.getAllByRole("img");
+
+    fireEvent.click(images[1]);
+
+    expect(onClickThumb).toHaveBeenCalledTimes(1);
+    expect(onClickThumb).toHaveBeenCalledWith(expect.any(Object), imgList[1]);
+  });
 });
