57591485a0ed71c6485d857598167f7c7c371f42
sprout2000 <wassabie64@gmail.com>
2024-06-02 09:23:58 +0900

    refactor: Update setAboutPanelOptions.copyright

--- src/main.ts
+++ src/main.ts
@@ -268,7 +268,7 @@ app.setAboutPanelOptions({
     : `v${app.getVersion()} (Electron v${process.versions.electron})`,
   version: `Electron v${process.versions.electron}`,
   iconPath: path.resolve(getResourceDirectory(), "images/logo.png"),
-  copyright: "© 2020-2024 sprout2000",
+  copyright: `© 2020-${new Date().getFullYear()} sprout2000`,
 });
 
 app.once("window-all-closed", () => app.exit());
